1d5212b054de11853a188718d92eeb3a8101f70f
john.boyer@jjb-software.com <john.boyer@jjb-software.com@20cf58a0-8c24-11dd-a14b-9ba93c563a2f>
2009-02-11 00:10:54 +0000

    putting in the new paths.c, which finds where the program is in Windows.
    git-svn-id: http://liblouisxml.googlecode.com/svn/trunk@52 20cf58a0-8c24-11dd-a14b-9ba93c563a2f

--- liblouisxml/paths.c
+++ liblouisxml/paths.c
@@ -11,17 +11,17 @@
    All rights reserved
 
    This file is free software; you can redistribute it and/or modify it
-   under the terms of the Lesser or Library GNU General Public License 
+   under the terms of the Lesser or Library GNU General Public License
    as published by the
    Free Software Foundation; either version 3, or (at your option) any
    later version.
 
    This file is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    Library GNU General Public License for more details.
 
-   You should have received a copy of the Library GNU General Public 
+   You should have received a copy of the Library GNU General Public
    License along with this program; see the file COPYING.  If not, write to
    the Free Software Foundation, 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.
@@ -34,38 +34,90 @@
 #include <string.h>
 #include "louisxml.h"
 
-/* Contributed by Yumei Sun of ViewPlus Technologies, Inc.*/
+/* Contributed by Michel Such <michel.such@free.fr */
 #ifdef _WIN32
-#include <Shlobj.h>
-// get the path to Program Files
-static char progFilesDir[MAXNAMELEN];
-typedef HRESULT (CALLBACK * LPFNDLLFUNC1) (HWND, int, HANDLE, DWORD, LPTSTR);
 
-void
-getProgFilesDir ()
+/* Adapted from BRLTTY code (see sys_progs_wihdows.h) */
+
+#include <shlobj.h>
+
+static void
+noMemory (void)
 {
+  printf ("Insufficient memory: %s", strerror (errno), "\n");
+  exit (3);
+}
 
-  // Get Start Menu\Programs directory
-  HMODULE hModSHFolder = LoadLibraryA ("shfolder.dll");
-  if (hModSHFolder != NULL)
+void *
+reallocWrapper (void *address, size_t size)
+{
+  if (!(address = realloc (address, size)) && size)
+    noMemory ();
+  return address;
+}
+
+char *
+strdupWrapper (const char *string)
+{
+  char *address = strdup (string);
+  if (!address)
+    noMemory ();
+  return address;
+}
+
+char *
+getProgramPath (void)
+{
+  char *path = NULL;
+  HMODULE handle;
+
+  if ((handle = GetModuleHandle (NULL)))
     {
-      LPFNDLLFUNC1 lpfnSHGetFolderPath =
-	(LPFNDLLFUNC1) GetProcAddress (hModSHFolder, "SHGetFolderPathA");
-      if (lpfnSHGetFolderPath != NULL)
-	{
-	  (*lpfnSHGetFolderPath) (NULL, 0x0026, NULL, NULL, progFilesDir);
+      size_t size = 0X80;
+      char *buffer = NULL;
 
+      while (1)
+	{
+	  buffer = reallocWrapper (buffer, size <<= 1);
+
+	  {
+	    DWORD length = GetModuleFileName (handle, buffer, size);
+
+	    if (!length)
+	      {
+		printf ("GetModuleFileName\n");
+		exit (3);
+		3;
+	      }
+
+	    if (length < size)
+	      {
+		buffer[length] = 0;
+		path = strdupWrapper (buffer);
+
+		while (length > 0)
+		  if (path[--length] == '\\')
+		    break;
+
+		strncpy (path, path, length + 1);
+		path[length + 1] = '\0';
+		break;
+	      }
+	  }
 	}
-      FreeLibrary (hModSHFolder);
+
+      free (buffer);
     }
   else
     {
-      strcpy (progFilesDir, "C:\\Program Files\\");
+      printf ("GetModuleHandle\n");
+      exit (3);
     }
-}
 
+  return path;
+}
 #endif
-/* End of YS contribution */
+/* End of MS contribution */
 
 static char *lastPath = NULL;
 
@@ -109,11 +161,20 @@ set_paths (const char *configPath)
 /*Set other paths here*/
 #ifdef _WIN32
 /* Set Windows Paths */
-  getProgFilesDir ();
-  strcpy (currentPath, progFilesDir);
-  strcat (currentPath, yourSubDir);
-  if (!addPath (currentPath))
-    return 0;
+  {
+    char louisPath[MAXNAMELEN];
+    char lbxPath[MAXNAMELEN];
+    strcpy (currentPath, getProgramPath ());
+    strcat (currentPath, "..\\");
+    if (!addPath (currentPath))
+      return 0;
+    strcpy (lbxPath, currentPath);
+    strcat (lbxPath, "share\\liblouisxml\\lbx_files\\");
+    addPath (lbxPath);
+    strcpy (louisPath, currentPath);
+    strcat (louisPath, "share\\liblouis\\tables\\");
+    addPath (louisPath);
+  }
 #else
 /* Set Unix paths (Linux, Mac OS X, etc.) */
   addPath (LIBLOUIS_TABLES_PATH);

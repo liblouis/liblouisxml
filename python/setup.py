bb91881c7ffd87677ad5f4e94cef6bece1e23ece
john.boyer@jjb-software.com <john.boyer@jjb-software.com@20cf58a0-8c24-11dd-a14b-9ba93c563a2f>
2009-11-25 17:10:47 +0000

    Adding python bindings
    git-svn-id: http://liblouisxml.googlecode.com/svn/trunk@189 20cf58a0-8c24-11dd-a14b-9ba93c563a2f

---
+++ python/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(
+    name = "louisxml",
+    version = "1.0",
+    url = "http://pypi.python.org/pypi/louisxml",
+    description = """Python interface for liblouisxml""",
+    long_description =
+        """
+        Python interface for liblouisxml
+        =======================================
+        
+        This is a set of python bindings for the liblouisxml Braille translation 
+        library. It aims to try and keep to the liblouisxml API, only
+        changing things where it makes more sense and is simpler for use in
+        python.
+        """,
+    author = "Michael Whapples",
+    author_email = "mwhapples@users.berlios.de",
+    ext_modules = [Extension("louisxml", ["louisxml.c"], libraries=["louisxml"])],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Artistic License",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Hardware",
+        "Operating System :: POSIX"
+    ]
+)
